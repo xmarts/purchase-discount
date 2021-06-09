d5fdac37672564f20b5f4525e3e7d70b63e3609f
Elymar <elymar.alfaro@gmail.com>
2021-06-09 12:29:16 -0500

    [ADD]Purchase Discount V14

--- purchase_discount/models/product_supplierinfo.py
+++ purchase_discount/models/product_supplierinfo.py
@@ -2,38 +2,34 @@
 # Copyright 2014-2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields, api
-import odoo.addons.decimal_precision as dp
+from odoo import api, fields, models
 
 
 class ProductSupplierInfo(models.Model):
-    _inherit = 'product.supplierinfo'
+    _inherit = "product.supplierinfo"
 
-    discount = fields.Float(
-        string='Discount (%)', digits=dp.get_precision('Discount'))
+    discount = fields.Float(string="Discount (%)", digits="Discount")
 
-    @api.onchange('name')
-    @api.multi
+    @api.onchange("name")
     def onchange_name(self):
         """ Apply the default supplier discount of the selected supplier """
-        for supplierinfo in self.filtered('name'):
-            supplierinfo.discount =\
-                supplierinfo.name.default_supplierinfo_discount
+        for supplierinfo in self.filtered("name"):
+            supplierinfo.discount = supplierinfo.name.default_supplierinfo_discount
 
     @api.model
     def _get_po_to_supplierinfo_synced_fields(self):
         """Overwrite this method for adding other fields to be synchronized
         with product.supplierinfo.
         """
-        return ['discount']
+        return ["discount"]
 
     @api.model_create_multi
     def create(self, vals_list):
-        """ Insert discount (or others) from context from purchase.order's
-        _add_supplier_to_product method """
+        """Insert discount (or others) from context from purchase.order's
+        _add_supplier_to_product method"""
         for vals in vals_list:
-            product_tmpl_id = vals['product_tmpl_id']
-            po_line_map = self.env.context.get('po_line_map', {})
+            product_tmpl_id = vals["product_tmpl_id"]
+            po_line_map = self.env.context.get("po_line_map", {})
             if product_tmpl_id in po_line_map:
                 po_line = po_line_map[product_tmpl_id]
                 for field in self._get_po_to_supplierinfo_synced_fields():
--- purchase_discount/models/purchase_order.py
+++ purchase_discount/models/purchase_order.py
@@ -3,46 +3,46 @@
 # Copyright 2015-2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+
 from odoo import api, fields, models
-import odoo.addons.decimal_precision as dp
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
-    @api.multi
     def _add_supplier_to_product(self):
-        """ Insert a mapping of products to PO lines to be picked up
-        in supplierinfo's create() """
+        """Insert a mapping of products to PO lines to be picked up
+        in supplierinfo's create()"""
         self.ensure_one()
-        po_line_map = dict([
-            (line.product_id.product_tmpl_id.id, line)
-            for line in self.order_line
-        ])
-        return super(PurchaseOrder, self.with_context(
-            po_line_map=po_line_map))._add_supplier_to_product()
+        po_line_map = {
+            line.product_id.product_tmpl_id.id: line for line in self.order_line
+        }
+        return super(
+            PurchaseOrder, self.with_context(po_line_map=po_line_map)
+        )._add_supplier_to_product()
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
     # adding discount to depends
-    @api.depends('discount')
+    @api.depends("discount")
     def _compute_amount(self):
         return super()._compute_amount()
 
     def _prepare_compute_all_values(self):
         vals = super()._prepare_compute_all_values()
-        vals.update({'price_unit': self._get_discounted_price_unit()})
+        vals.update({"price_unit": self._get_discounted_price_unit()})
         return vals
 
-    discount = fields.Float(
-        string='Discount (%)', digits=dp.get_precision('Discount'),
-    )
+    discount = fields.Float(string="Discount (%)", digits="Discount")
 
     _sql_constraints = [
-        ('discount_limit', 'CHECK (discount <= 100.0)',
-         'Discount must be lower than 100%.'),
+        (
+            "discount_limit",
+            "CHECK (discount <= 100.0)",
+            "Discount must be lower than 100%.",
+        )
     ]
 
     def _get_discounted_price_unit(self):
@@ -57,7 +57,6 @@ class PurchaseOrderLine(models.Model):
             return self.price_unit * (1 - self.discount / 100)
         return self.price_unit
 
-    @api.multi
     def _get_stock_move_price_unit(self):
         """Get correct price with discount replacing current price_unit
         value before calling super and restoring it later for assuring
@@ -77,7 +76,7 @@ class PurchaseOrderLine(models.Model):
             self.price_unit = price_unit
         return price
 
-    @api.onchange('product_qty', 'product_uom')
+    @api.onchange("product_qty", "product_uom")
     def _onchange_quantity(self):
         """
         Check if a discount is defined into the supplier info and if so then
@@ -89,8 +88,11 @@ class PurchaseOrderLine(models.Model):
             if self.order_id.date_order:
                 date = self.order_id.date_order.date()
             seller = self.product_id._select_seller(
-                partner_id=self.partner_id, quantity=self.product_qty,
-                date=date, uom_id=self.product_uom)
+                partner_id=self.partner_id,
+                quantity=self.product_qty,
+                date=date,
+                uom_id=self.product_uom,
+            )
             self._apply_value_from_seller(seller)
         return res
 
@@ -101,3 +103,35 @@ class PurchaseOrderLine(models.Model):
         if not seller:
             return
         self.discount = seller.discount
+
+    def _prepare_account_move_line(self, move=False):
+        vals = super(PurchaseOrderLine, self)._prepare_account_move_line(move)
+        vals["discount"] = self.discount
+        return vals
+
+    @api.model
+    def _prepare_purchase_order_line(
+        self, product_id, product_qty, product_uom, company_id, supplier, po
+    ):
+        """Apply the discount to the created purchase order"""
+        res = super()._prepare_purchase_order_line(
+            product_id, product_qty, product_uom, company_id, supplier, po
+        )
+        partner = supplier.name
+        uom_po_qty = product_uom._compute_quantity(product_qty, product_id.uom_po_id)
+        seller = product_id.with_company(company_id)._select_seller(
+            partner_id=partner,
+            quantity=uom_po_qty,
+            date=po.date_order and po.date_order.date(),
+            uom_id=product_id.uom_po_id,
+        )
+        res.update(self._prepare_purchase_order_line_from_seller(seller))
+        return res
+
+    @api.model
+    def _prepare_purchase_order_line_from_seller(self, seller):
+        """Overload this function to prepare other data from seller,
+        like in purchase_triple_discount module"""
+        if not seller:
+            return {}
+        return {"discount": seller.discount}
