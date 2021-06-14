da018fb5559d85f7ab073bb2c691fc188d45c5c2
jesus alvarez <jesus.alvarez.xmarts@gmail.com>
2021-06-14 10:37:30 -0400

    fix

--- purchase_discount/models/product_template.py
+++ purchase_discount/models/product_template.py
@@ -6,21 +6,20 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
     descuento = fields.Float(related="categ_id.category_discount")
-    
-    @api.depends("categ_id.parent_id.category_discount", "categ_id.category_discount", "seller_ids")
+    descuento_padre = fields.Float(related="categ_id.descuento_padre")
+
+    @api.depends("categ_id.parent_id.category_discount", "categ_id.category_discount", "seller_ids","seller_ids.price")
     def _compute_category_discount(self):
         for rec in self:
-            categ_discount = 0
-            for categ in rec.categ_id:
-                descuento = categ.category_discount
-                descuento_padre = categ.parent_id.category_discount
-                if descuento:
-                    categ_discount = descuento
-                elif descuento_padre:
-                    categ_discount = descuento_padre
-            for line in seller_ids:
-                self.env['product.supplierinfo'].write(
-                    {
-                        'discount': categ_discount
-                    }
-                )
+            discount = rec.descuento
+            parent_discount = rec.descuento_padre
+            if descuento:
+                categ_discount = discount
+            elif descuento_padre:
+                categ_discount = parent_discount
+        for line in seller_ids:
+            self.env['product.supplierinfo'].write(
+                {
+                    'discount': categ_discount
+                }
+            )
