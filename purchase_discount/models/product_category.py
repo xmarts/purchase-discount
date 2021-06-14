27bbdeb22c198c6c35a275f34030b3d892d646a3
jesus alvarez <jesus.alvarez.xmarts@gmail.com>
2021-06-14 09:17:13 -0400

    fix

--- purchase_discount/models/product_category.py
+++ purchase_discount/models/product_category.py
@@ -4,5 +4,16 @@ from odoo import api, fields, models, tools, _
 class ProductCategory(models.Model):
     _inherit = "product.category"
 
-    category_discount =  fields.Float(string="Category Discount (%)", default=0)
+    category_discount =  fields.Float(string="Category Discount (%)")
 
+    descuento_padre = fields.Float(related="categ_id.category_discount")
+    
+    @api.depends("categ_id.parent_id.category_discount", "categ_id.category_discount", "seller_ids")
+    def _compute_category_discount(self):
+        for rec in self:
+            categ_discount = 0
+            for categ in rec.categ_id:
+                descuento = categ.parent_id.category_discou
+                if descuento:
+                    categ_discount = descuento_padre
+            rec.descuento_padre = categ_discount
