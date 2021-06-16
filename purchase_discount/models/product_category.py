fb25b8be5318b3f910ce4eb77560b25af86f40b1
jesus alvarez <jesus.alvarez.xmarts@gmail.com>
2021-06-16 12:47:34 -0400

    fix

--- purchase_discount/models/product_category.py
+++ purchase_discount/models/product_category.py
@@ -16,3 +16,10 @@ class ProductCategory(models.Model):
             if descuento:
                 categ_discount = descuento
         rec.descuento_padre = categ_discount
+
+    @api.depends("category_discount")
+    def _env_category_discount(self):
+        for rec in self:
+            dicount = rec.category_discount
+            if discount:
+                self.env["product.template"].search([('categ_id', '=', self.id)]).write("descuento":discount)
--- purchase_discount/models/product_template.py
+++ purchase_discount/models/product_template.py
@@ -5,5 +5,5 @@ from odoo import fields, models, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
-    descuento = fields.Float(related="categ_id.category_discount")
+    descuento = fields.Float()
     descuento_padre = fields.Float(related="categ_id.descuento_padre")
