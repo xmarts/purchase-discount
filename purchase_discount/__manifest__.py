b8872b15aabd1cd78e77137f6a0c125b05734a10
Pedro Noriega <pedro.noriega@xmarts.com>
2026-07-03 12:01:22 -0400

    [FIX]

--- purchase_discount/__manifest__.py
+++ purchase_discount/__manifest__.py
@@ -10,19 +10,23 @@
     "ACSONE SA/NV,"
     "GRAP,"
     "Odoo Community Association (OCA)",
-    "version": "19.0",
+    "version": "19.0.1.0.0",
     "category": "Purchase Management",
     "website": "https://github.com/OCA/purchase-workflow",
     "depends": [
         "purchase_stock"
     ],
     "data": [
-        "data/fix_views.xml",
-        #"views/purchase_discount_view.xml",
-        #"views/report_purchaseorder.xml",
-        #"views/product_supplierinfo_view.xml",
-        #"views/res_partner_view.xml",
+        # The migration helper below performs global view rewrites and can
+        # break registry loading on newer versions. Keep it disabled in v19.
+        # "data/fix_views.xml",
+        # "views/purchase_discount_view.xml",
+        # "views/report_purchaseorder.xml",
+        # "views/product_supplierinfo_view.xml",
+        # "views/res_partner_view.xml",
     ],
     "license": "AGPL-3",
     "images": ["images/purchase_discount.png"],
+    "installable": True,
+    "application": False,
 }
